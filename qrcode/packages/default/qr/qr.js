95bcf205121f89dd392310e0939103e1aa339f15
Davi DeBarros <davibarros1994@gmail.com>
2022-02-14 16:00:34 -0500

    Update qr.js
    Adds a console log for args

--- qrcode/packages/default/qr/qr.js
+++ qrcode/packages/default/qr/qr.js
@@ -2,6 +2,7 @@ const qrcode = require('qrcode')
 
 exports.main = (args) => {
   return qrcode.toDataURL(args.text).then(res => ({
+    console.log(args)
     headers:  { 'content-type': 'text/html; charset=UTF-8' },
     body: args.img == undefined ? res : `<img src="${res}">`
   }))
